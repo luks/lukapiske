37b199e6ad8fa0ca1a3ff91e17f4404ef34358fe
Luky <lukapiske@gmail.com>
2016-04-30 10:34:19 +0200

    Redis commands

--- redis.rb
+++ redis.rb
@@ -1,7 +1,5 @@
 
-
- redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 14)
-
+redis = Redis.new(:host => "127.0.0.1", :port => 6379, :db => 14)
 
 redis.zcard "score"  // count
 
@@ -13,20 +11,13 @@ redis.zscan("score", 0, :match => "*ping*.ng.*", :count => 1600)
 
 redis.zscan score 0  match *.ng.* count 50000
 
-
 redis.zrevrange hackers 0 -1
 
 redis.zrevrange "score", 0, -1, :withscores=>true
 
-
 redis.zscan("score", 0 , :match => "*ssh*", :count=>50000)
 
-
-
-
-
 redis.hscan("target",0 , :match => "*admins*", :count=>500)
-
 
 redis.hscan("target",0 , :match => "*.go.*", :count=>50000)
 
@@ -36,7 +27,6 @@ redis.hvals("target")
 
 a=[];redis.hvals("target").each {|x| a << x if x =~ /luka/}
 
-
 redis.hset "revision", "num", 0
 
 redis.hincrby 'revision', 'num', 1
@@ -45,10 +35,21 @@ redis.hget "revision", "num"
 
 redis.hmset("puzzle.ng.seznam.cz", "ntp_score", "220", "ntp_status", "something realy nasty happened", "")
 
-Every Redis command is atomic, HMSET as well.
+# Every Redis command is atomic, HMSET as well.
 
-Sets the specified fields to their respective values in the hash stored at key. This command overwrites any existing fields in the hash. If key does not exist, a new key holding a hash is created.
+# Sets the specified fields to their respective values in the hash stored at key. This command overwrites any existing fields in the 
+# hash. #If key does not exist, a new key holding a hash is created.
 
 redis.hmset("puzzle.ng.seznam.cz", "ntp_score", "223", "ntp_status", "something realy nasty happened")
 
 redis.hscan "puzzle.ko.seznam.cz", 0, :match=>"ntp_score"
+
+a = [];redis.hkeys("target").each { |x| a << x }
+
+redis.hmget( "target", a[0,100])
+
+
+
+
+
+
